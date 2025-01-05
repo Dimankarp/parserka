9d7cc3a34f5dadf834d88b168e730b0f64a5cc38
Dimankarp <marinad1814@yandex.ru>
2025-01-05 22:37:28 +0300

    feat(core): Added core parts of a parsec-like parser combinator

---
+++ src/Parserka/Parser.hs
@@ -0,0 +1,90 @@
+{-# LANGUAGE InstanceSigs #-}
+{-# OPTIONS_GHC -Wno-unused-do-bind #-}
+
+module Parserka.Parser (char) where
+
+import Data.Char (isAlpha, isDigit)
+
+newtype Parser i a = Parser
+  { runParser :: [i] -> Consumed a i
+  }
+
+instance Functor (Parser i) where
+  fmap :: (a -> b) -> Parser i a -> Parser i b
+  fmap f m = m >>= return . f
+
+instance Applicative (Parser i) where
+  pure x = Parser $ \i -> Empty (Ok x i)
+
+  m1 <*> m2 = m1 >>= (\x1 -> m2 >>= (\x2 -> return (x1 x2)))
+
+instance Monad (Parser i) where
+  return = pure
+  p >>= f =
+    Parser
+      ( \i -> case (runParser p i) of
+          Empty r1 ->
+            case (r1) of
+              Ok x rest -> (runParser (f x) rest)
+              Error -> Empty Error
+          Consumed r1 ->
+            Consumed
+              ( case (r1) of
+                  Ok x rest -> case (runParser (f x) rest) of
+                    Consumed r2 -> r2
+                    Empty r2 -> r2
+                  Error -> Error
+              )
+      )
+
+data Consumed a i = Consumed (Reply a i) | Empty (Reply a i) deriving (Show)
+
+data Reply a i = Ok a [i] | Error deriving (Show)
+
+satisfy :: (i -> Bool) -> Parser i i
+satisfy test = Parser $ \i -> case (i) of
+  [] -> Empty Error
+  (c : cs)
+    | test c -> Consumed (Ok c cs)
+    | otherwise -> Empty Error
+
+char :: (Eq i) => i -> Parser i i
+char c = satisfy (== c)
+
+letter :: Parser Char Char
+letter = satisfy isAlpha
+
+digit :: Parser Char Char
+digit = satisfy isDigit
+
+(<|>) :: Parser i a -> Parser i a -> Parser i a
+p <|> q =
+  Parser
+    ( \i -> case (runParser p i) of
+        Empty Error -> (runParser q i)
+        Empty ok -> case (runParser q i) of
+          Empty _ -> Empty ok
+          consumed -> consumed
+        consumed -> consumed
+    )
+
+string :: (Eq i) => [i] -> Parser i ()
+string [] = return ()
+string (i : is) = do
+  char i
+  string is
+
+idPart = letter <|> digit
+
+stop = Parser $ go
+  where
+    go [] = Empty (Ok () [])
+    go _ = Empty Error
+
+try :: Parser i a -> Parser i a
+try p = Parser $
+  \i -> case (runParser p i) of
+    Consumed Error -> Empty Error
+    other -> other
+
+identificator = try (do idPart; stop) <|> (do idPart; identificator)
